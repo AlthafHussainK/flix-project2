7a0f4ceccd8ea95fc519e991b7929a407c0ad218
AlthafHussainK <althafakm@gmail.com>
2021-04-28 03:21:42 +0530

    set jumbotron

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components/macro'
 
+export const Item = styled.div`
+  display: flex;
+  border-bottom: 8px solid #222;
+  padding: 50px 5%;
+  color: white;
+  overflow: hidden;
+`;
+
 
 export const Inner = styled.div`
   display: flex;
@@ -14,4 +22,37 @@ export const Inner = styled.div`
     flex-direction: column;
   }
 
-`
+`;
+
+export const Pane = styled.div`
+  width: 50%;
+
+  @media (max-width: 1000px) {
+    width: 100%;
+    padding: 0 45px;
+    text-align: center;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 50px;
+  line-height: 1.1;
+  margin-bottom: 8px;
+
+  @media (max-width: 600px) {
+    font-size: 35px;
+  }
+`;
+
+export const SubTitle = styled.h2`
+  fonst-size: 26px;
+  font-weight: normal;
+  line-height: normal;
+`;
+
+export const Image = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
+
+export const Container = styled.div``;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'normalize.css';
 import App from './App';
+import { GlobalStyles } from './global-styles';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<>
+  <GlobalStyles />
+  <App />
+  </>, 
+  document.getElementById('root'));
