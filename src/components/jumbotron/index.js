5158dcbe8f1a7af3470e84aa023b2bf3a053cd7b
AlthafHussainK <althafakm@gmail.com>
2021-04-28 07:06:35 +0530

    jumbotron Components

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -4,8 +4,8 @@ import { Container, Inner, Item, Pane, Title, SubTitle, Image } from './styles/j
 
 export default function Jumbotron({ children, direction = 'row', ...restProps}) {
   return (
-    <Item direction={direction}>
-      <Inner>
+    <Item {...restProps}>
+      <Inner direction={direction} >
         {children}
       </Inner>
     </Item>
