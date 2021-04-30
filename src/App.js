43d9f67f4e87c4e0b042343bf2f3e92cc09da147
AlthafHussainK <althafakm@gmail.com>
2021-04-30 06:52:09 +0530

    updates:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Home, Browse, Signin, Signup } from './pages'
 import * as ROUTES from './constants/routes'
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
+import { useAuthListener } from './hooks'
 
 
 export default function App() {
+  const { user } = useAuthListener()
+
   return (
     <Router>
-      <Route exact path="/browse">
-        <Browse />
-      </Route>
-      <Route exact path="/signin">
+    <Switch>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} >
         <Signin />
-      </Route>
-      <Route exact path="/signup">
+      </IsUserRedirect>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} >
         <Signup />
-      </Route>
-      <Route exact path={ROUTES.HOME}>
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <Browse />
+      </ProtectedRoute>
+      
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
         <Home />
-      </Route>
+      </IsUserRedirect>
+      </Switch>
     </Router>
   );
 }
