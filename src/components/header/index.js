d1174276b71ee1878ea8d1e1372f34ae56fb25e5
AlthafHussainK <althafakm@gmail.com>
2021-04-30 06:52:53 +0530

    update header styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import { Link as ReactRouterLink } from 'react-router-dom'
 
-import { Background, ButtonLink, Container, Logo, Feature, FeatureCallOut, Text } from './styles/header'
+import { 
+  Background, 
+  ButtonLink, 
+  Container, 
+  Link, 
+  Logo, 
+  Group, 
+  Profile, 
+  Dropdown,
+  Picture, 
+  Feature, 
+  FeatureCallOut, 
+  Text 
+} from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps}) {
   return bg ? <Background { ...restProps}>{children}</Background> : children;
@@ -15,12 +28,33 @@ Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }
   return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>
 }
 
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+  return <Profile {...restProps}>{children}</Profile>
+}
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+  return <Picture {...restProps} src={`/images/users/${src}.png`} />
+}
+
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+  return <Dropdown {...restProps}>{children}</Dropdown>
+}
+
 Header.Text = function HeaderText({ children, ...restProps }) {
   return <Text {...restProps}>{children}</Text>
 }
 
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>
+}
+
 Header.Frame = function HeaderFrame({ children, ...restProps }) {
   return <Container { ...restProps}>{children}</Container>
+}
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+  return <Group { ...restProps}>{children}</Group>
 }
 
 Header.ButtonLink = function HeaderButtonLink({ children, ...restProps }) {
--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -6,9 +6,19 @@ export const Background = styled.div `
   flex-direction: column;
   background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : 
                   '../images/misc/home-bg.jpg')}) top left / cover no-repeat;
+
+  @media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) => 
+      dontShowOnSmallViewPort && `background: none`}
+  }
 `;
 
 export const Frame = styled.div ``;
+
+export const Group = styled.div `
+  display: flex;
+  align-items: center;
+`;
 
 export const Container = styled.div `
   display: flex;
@@ -24,6 +34,83 @@ export const Container = styled.div `
 
   @media (max-width: 1000px) {
     margin: 0 30px;
+  }
+`;
+
+export const Picture = styled.button `
+  background: url(${({ src }) => src});
+  background-size: contain;
+  border: 0;
+  width: 32px;
+  height: 32px;
+  cursor: pointer;
+`;
+
+export const Link = styled.p `
+  color: white;
+  text-decoration: none;
+  margin-right: 30px;
+  font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
+  cursor: pointer;
+
+  &:hover {
+    font-weight: bold;
+  }
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
+
+export const Dropdown = styled.div `
+  display: none;
+  background-color: black;
+  position: absolute;
+  padding: 10px;
+  width: 100px;
+  top: 32px;
+  right: 10px;
+
+  ${Group}:last-of-type ${Link} {
+    cursor: pointer;
+  }
+
+  ${Group} {
+    margin-bottom: 10px;
+
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+
+    ${Link}, ${Picture} {
+      cursor: default;
+    }
+  }
+
+  button {
+    margin-right: 10px;
+  }
+
+  p {
+    font-size: 12px;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+`;
+
+export const Profile = styled.div `
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  position: relative;
+
+  button {
+    cursor: pointer;
+  }
+
+  &:hover > ${Dropdown} {
+    display: flex;
+    flex-direction: column;
   }
 `;
 
@@ -75,6 +162,7 @@ export const Text = styled.p `
   text-shadow: 2px 2px 4px rgba(0,0,0,.45);
   margin: 0;
 `;
+
 
 export const FeatureCallOut = styled.h2 `
   color: white;
