1b71c04887cad2b003b552f1caae8db1d6e47b40
AlthafHussainK <althafakm@gmail.com>
2021-04-30 06:53:32 +0530

    updates:

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { BrowseContainer } from '../containers/browse'
+import { useContent } from '../hooks'
+import selectionFilter from '../utils/selection-filter'
 
 export default function Browse() {
-  return <p>Hello from Browse</p>
+  const { series } = useContent('series')
+  const { films } = useContent('films')
+  
+  const slides = selectionFilter({ series, films })
+
+  return <BrowseContainer slides={slides} />
 }
