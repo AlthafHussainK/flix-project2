a5c498f330f558c9e416138f4df55e436dd8e113
AlthafHussainK <althafakm@gmail.com>
2021-05-02 23:50:49 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
-import { Home, Browse, Signin, Signup } from './pages'
+import { Home, Browse, SignIn, SignUp } from './pages'
 import * as ROUTES from './constants/routes'
 import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 import { useAuthListener } from './hooks'
 
 
-export default function App() {
+export function App() {
   const { user } = useAuthListener()
 
   return (
     <Router>
-    <Switch>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} >
-        <Signin />
-      </IsUserRedirect>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} >
-        <Signup />
-      </IsUserRedirect>
-      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
-        <Browse />
-      </ProtectedRoute>
-      
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact>
-        <Home />
-      </IsUserRedirect>
-      </Switch>
+      <Switch>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} >
+          <SignIn />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} >
+          <SignUp />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE}>
+          <Browse />
+        </ProtectedRoute>
+        
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME}>
+          <Home />
+        </IsUserRedirect>
+        </Switch>
     </Router>
   );
 }
