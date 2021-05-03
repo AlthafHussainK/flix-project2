b2e13e3ca90cd15d9362655f735f413c6fd06cae
AlthafHussainK <althafakm@gmail.com>
2021-05-03 05:40:46 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import 'normalize.css';
-import { App } from './App';
 import { GlobalStyles } from './global-styles';
-import { firebase } from './lib/firebase.prod'
-import { FirebaseContext } from './context/firebase'
+import { App } from './App';
+import { firebase } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <FirebaseContext.Provider value={{ firebase }}>
       <GlobalStyles />
       <App />
     </FirebaseContext.Provider>
-  </React.StrictMode>, 
+  </React.StrictMode>,
   document.getElementById('root')
 );
