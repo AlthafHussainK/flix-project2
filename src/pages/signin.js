9fed5ac153c868f7c2341489325571bfa2ce1892
AlthafHussainK <althafakm@gmail.com>
2021-05-03 05:38:56 +0530

    updates

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,27 +1,27 @@
-import React, { useState, useContext } from 'react'
-import { useHistory } from 'react-router-dom'
-import { FirebaseContext } from '../context/firebase'
-import { HeaderContainer } from '../containers/header'
-import { FooterContainer } from '../containers/footer'
-import { Form } from '../components'
-import * as ROUTES from '../constants/routes'
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
 export default function SignIn() {
-  const history = useHistory()
-  const { firebase } = useContext(FirebaseContext)
-  
-  const [emailAddress, setEmailAddress] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleSignIn = (event) => {
+  const handleSignin = (event) => {
     event.preventDefault();
 
     return firebase
       .auth()
-      .signInWithEmailAndPassword(emailAddress, password).then(() => {console.log("SIGNED IN")})
+      .signInWithEmailAndPassword(emailAddress, password)
       .then(() => {
         history.push(ROUTES.BROWSE);
       })
@@ -37,9 +37,9 @@ export default function SignIn() {
       <HeaderContainer>
         <Form>
           <Form.Title>Sign In</Form.Title>
-          {error && <Form.Error>{error}</Form.Error>}
+          {error && <Form.Error data-testid="error">{error}</Form.Error>}
 
-          <Form.Base onSubmit={handleSignIn} method="POST">
+          <Form.Base onSubmit={handleSignin} method="POST">
             <Form.Input
               placeholder="Email address"
               value={emailAddress}
@@ -47,9 +47,9 @@ export default function SignIn() {
             />
             <Form.Input
               type="password"
-              placeholder="Password"
-              autoComplete="off"
               value={password}
+              autoComplete="off"
+              placeholder="Password"
               onChange={({ target }) => setPassword(target.value)}
             />
             <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-in">
@@ -58,15 +58,14 @@ export default function SignIn() {
           </Form.Base>
 
           <Form.Text>
-            New to Netflix? <Form.Link to="/signup">Sign Up now.</Form.Link>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
           </Form.Text>
           <Form.TextSmall>
             This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
           </Form.TextSmall>
         </Form>
-
       </HeaderContainer>
       <FooterContainer />
     </>
-  )
+  );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,46 +1,45 @@
-import React, { useState, useContext } from 'react'
-import { useHistory } from 'react-router-dom'
-import { FirebaseContext } from '../context/firebase'
-import { HeaderContainer } from '../containers/header'
-import { FooterContainer } from '../containers/footer'
-import { Form } from '../components'
-import * as ROUTES from '../constants/routes'
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/routes';
 
 export default function SignUp() {
-  const history = useHistory()
-  const { firebase } = useContext(FirebaseContext)
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
 
-  const [firstName, setFirstName] = useState('')
-  const [emailAddress, setEmailAddress] = useState('')
-  const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
+  const [firstName, setFirstName] = useState('');
+  const [emailAddress, setEmailAddress] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
   const isInvalid = firstName === '' || password === '' || emailAddress === '';
 
-  const handleSignUp = (event) => {
-    event.preventDefault()
+  const handleSignup = (event) => {
+    event.preventDefault();
 
-    firebase  
+    return firebase
       .auth()
       .createUserWithEmailAndPassword(emailAddress, password)
-      .then((result) => 
-        result.user.updateProfile({
-          displayName: firstName,
-          photoURL: Math.floor(Math.random() * 5) + 1
-        })
-        .then(() => {
-          history.push(ROUTES.BROWSE)
-        })
+      .then((result) =>
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          })
       )
       .catch((error) => {
         setFirstName('');
         setEmailAddress('');
         setPassword('');
-        setError('')
-        setError(error.message)
-      })
-
-  }
+        setError(error.message);
+      });
+  };
 
   return (
     <>
@@ -49,39 +48,38 @@ export default function SignUp() {
           <Form.Title>Sign Up</Form.Title>
           {error && <Form.Error>{error}</Form.Error>}
 
-          <Form.Base onSubmit={handleSignUp} method="POST">
+          <Form.Base onSubmit={handleSignup} method="POST">
             <Form.Input
               placeholder="First name"
               value={firstName}
               onChange={({ target }) => setFirstName(target.value)}
             />
-             <Form.Input
+            <Form.Input
               placeholder="Email address"
               value={emailAddress}
               onChange={({ target }) => setEmailAddress(target.value)}
             />
-            <Form.Input 
+            <Form.Input
               type="password"
               value={password}
               autoComplete="off"
               placeholder="Password"
-              onChange={({ target}) => setPassword(target.value)}
+              onChange={({ target }) => setPassword(target.value)}
             />
-            <Form.Submit disabled={isInvalid} type="submit">
+            <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-up">
               Sign Up
             </Form.Submit>
-
-            <Form.Text>
-              Already a user? <Form.Link to="/signin">Sign in now.</Form.Link>
-            </Form.Text>
-            <Form.TextSmall>
-              This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
-            </Form.TextSmall>
           </Form.Base>
+
+          <Form.Text>
+            Already a user? <Form.Link to="/signin">Sign in now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn more.
+          </Form.TextSmall>
         </Form>
       </HeaderContainer>
-    
       <FooterContainer />
     </>
-  )
+  );
 }
